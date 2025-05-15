fe4d7c6484392515980d29069097efade4e3ada2
josehlourenco <jose.h.lourenco@meo.pt>
2025-05-15 15:11:23 +0000

    Resolucao

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -2,3 +2,9 @@
 
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+
+INSERT or ignore into salario_classe(id, salario, nivel)
+VALUES
+(4, 3500, 'estagiario'),
+(5, 4500, 'iniciante - 1'),
+(6, 5500, 'iniciante - 2');
