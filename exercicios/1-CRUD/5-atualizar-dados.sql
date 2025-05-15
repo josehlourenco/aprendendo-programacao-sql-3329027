a0483becfe8fa558010291076873dec5984f4e82
josehlourenco <jose.h.lourenco@meo.pt>
2025-05-15 15:47:11 +0000

    resolucao

--- exercicios/1-CRUD/5-atualizar-dados.sql
+++ exercicios/1-CRUD/5-atualizar-dados.sql
@@ -1,5 +1,7 @@
 -- Atualize a classe salarial para a pessoa cujo EmployeeId é igual a 6. Lembre-se que a classe salarial deve corresponder o id_salario da tabela salario_classe
-
+UPDATE employees
+SET id_salario = 1
+WHERE EmployeeID = 6
 
 -- Atualize a classe salarial e o LastName da pessoa cujo EmployeeId é igual a 2. Lembre-se que a classe salarial deve corresponder o id_salario da tabela salario_classe
 
